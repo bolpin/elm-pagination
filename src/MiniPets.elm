d5011757511d3c6e91a4c53c62640d9eac14e63f
bolpin <bolpin@gettyimages.com>
2017-06-22 18:28:10 -0700

    adding an input

--- src/MiniPets.elm
+++ src/MiniPets.elm
@@ -1,29 +1,78 @@
--- Read more about this program in the official Elm guide:
--- https://guide.elm-lang.org/architecture/user_input/buttons.html
-
-
 module Main exposing (..)
 
-import Html exposing (beginnerProgram, div, button, text)
-import Html.Events exposing (onClick)
+import Html exposing (..)
+import Html.Attributes exposing (href, style, disabled, placeholder, autofocus, type_, value)
+import Html.Events exposing (onInput, onClick)
+import Regex exposing (caseInsensitive, regex, contains)
+
+
+-- Boilerplate wiring
 
 
 main =
-    beginnerProgram { model = init, view = view, update = update }
+    beginnerProgram
+        { model = init
+        , view = view
+        , update = update
+        }
+
+
+
+-- Model
 
 
 init =
-    { count = 0, level = 0 }
+    { count = 0
+    , level = 0
+    , noun = "Baby Duck"
+    }
+
+
+
+-- View
 
 
 view model =
     div []
         [ button [ onClick Decrement ] [ text "-" ]
-        , div [] [ text ((toString model.count) ++ (noun model.count) ++ excite model.level) ]
-        , button [ onClick Increment ] [ text "+" ]
+        , div []
+            [ text
+                ((toString model.count)
+                    ++ " "
+                    ++ (pluralize model.noun model.count)
+                    ++ excite model.level
+                )
+            ]
+        , button [ onClick Increment ]
+            [ text "+" ]
+        , input
+            [ type_ "text"
+            , placeholder "Filter by Name"
+            , value model.noun
+            , onInput UpdateNoun
+            , autofocus True
+            ]
+            []
         , button [ onClick Reset ] [ text "reset" ]
         , button [ onClick Excite ] [ text "make exciting" ]
         ]
+
+
+pluralize noun count =
+    case abs (count) of
+        1 ->
+            noun
+
+        _ ->
+            noun ++ "s"
+
+
+excite level =
+    String.repeat level "!!!"
+
+
+
+-- Update
 
 
 type Msg
@@ -31,23 +80,14 @@ type Msg
     | Decrement
     | Reset
     | Excite
-
-
-noun numPets =
-    case abs (numPets) of
-        1 ->
-            " Pet"
-
-        _ ->
-            " Pets"
-
-
-excite level =
-    String.repeat level "!!!"
+    | UpdateNoun String
 
 
 update msg model =
     case msg of
+        UpdateNoun newNoun ->
+            { model | noun = newNoun }
+
         Increment ->
             { model | count = model.count + 1 }
 
